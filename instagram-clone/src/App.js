0fae6b8456ce8e1edac6a1dbcb6bac5ba2d58a84
DeployChef <dc.titovvlad@gmail.com>
2020-09-17 00:07:52 +0300

    Add header

--- instagram-clone/src/App.js
+++ instagram-clone/src/App.js
@@ -3,8 +3,21 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div className="app">
+      
+      <div className="app__header">
+        <img alt="InstagramClone"
+         class="app__headerImage" 
+         src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"/>
+      </div>
+
+      <h1>Hello World 🚀🚀🚀</h1>
+
+
+      {/* Header */}
+
+      {/* Posts */}
+      {/* Posts */}
     </div>
   );
 }
