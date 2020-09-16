9ab20555fcb4d302320ee267f0b31286a319b778
DeployChef <dc.titovvlad@gmail.com>
2020-09-17 00:23:37 +0300

    add material ui

--- instagram-clone/src/Post.js
+++ instagram-clone/src/Post.js
@@ -1,15 +1,18 @@
 import React from 'react'
+import './Post.css'
 
 function Post() {
     return (
-        <div>
+        <div className="post">
+
             <h3>UpNova</h3>
             {/*Header -> avatar + username */}
 
-            <img src="https://lh3.googleusercontent.com/proxy/IXUw177pmDkIgfXXjZRy1LCBIntOruHEc5H1xm3wXZGcgKzWOf1F5U4sgNBKDwm0phYpQpq6757tCoqHB4Vl0ulXMZd3fI2NeS930V7KsBl6jXk"/>
+            <img className="post__image" src="https://lh3.googleusercontent.com/proxy/IXUw177pmDkIgfXXjZRy1LCBIntOruHEc5H1xm3wXZGcgKzWOf1F5U4sgNBKDwm0phYpQpq6757tCoqHB4Vl0ulXMZd3fI2NeS930V7KsBl6jXk"/>
              {/*image */}
 
             <h4>UpNova: caption</h4>
+            <h4 className="post__text"><strong>edya</strong> WOW Awesome</h4>
               {/*username + caption */}
         </div>
     )
