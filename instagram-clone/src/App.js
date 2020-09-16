3a972345eca4e97c0dfd29409d010778ca5d4818
DeployChef <dc.titovvlad@gmail.com>
2020-09-17 00:49:00 +0300

    Different posts from props

--- instagram-clone/src/App.js
+++ instagram-clone/src/App.js
@@ -14,11 +14,12 @@ function App() {
 
       <h1>Hello World 🚀🚀🚀</h1>
 
-      <Post></Post>
-      {/* Header */}
-
-      {/* Posts */}
-      {/* Posts */}
+      <Post imageUrl="https://lh3.googleusercontent.com/proxy/IXUw177pmDkIgfXXjZRy1LCBIntOruHEc5H1xm3wXZGcgKzWOf1F5U4sgNBKDwm0phYpQpq6757tCoqHB4Vl0ulXMZd3fI2NeS930V7KsBl6jXk"
+            username="UpNova"
+            caption="WoW Awesome"/>
+      <Post imageUrl="https://img-a.udemycdn.com/course/240x135/2180720_aa12_2.jpg"
+            username="DeployChef"
+            caption="I did an Instagram clone on React.js"/>
     </div>
   );
 }
--- instagram-clone/src/Post.js
+++ instagram-clone/src/Post.js
@@ -2,26 +2,25 @@ import React from 'react'
 import './Post.css'
 import Avatar from "@material-ui/core/Avatar"
 
-function Post() {
+function Post({username, caption, imageUrl}) {
     return (
         <div className="post">
 
             <div className="post__header">
                 <Avatar 
                     className="post__avatar"
-                    alt="UpNova"
+                    alt={username}
                     src=""
                 />
-                <h3>UpNova</h3>
+                <h3>{username}</h3>
             </div>
 
             {/*Header -> avatar + username */}
 
-            <img className="post__image" src="https://lh3.googleusercontent.com/proxy/IXUw177pmDkIgfXXjZRy1LCBIntOruHEc5H1xm3wXZGcgKzWOf1F5U4sgNBKDwm0phYpQpq6757tCoqHB4Vl0ulXMZd3fI2NeS930V7KsBl6jXk"/>
+            <img className="post__image" src={imageUrl}/>
              {/*image */}
 
-            <h4>UpNova: caption</h4>
-            <h4 className="post__text"><strong>edya</strong> WOW Awesome</h4>
+            <h4 className="post__text"><strong>{username}</strong> {caption}</h4>
               {/*username + caption */}
         </div>
     )
